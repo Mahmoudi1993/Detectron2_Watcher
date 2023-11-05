5b4c436e89458c4ebb31f9024b5fdb37bc1498d5
Reyhaneh_Mahmoudi <74957886+Mahmoudi1993@users.noreply.github.com>
2023-11-05 15:30:57 +0330

    Update Detector.py

--- Detector.py
+++ Detector.py
@@ -16,43 +16,40 @@ import supervision as sv
 from absl import app, flags, logging
 from absl.flags import FLAGS
 from Detection import predictor
-#from IPython import display
+
 
 FLAGS = flags.FLAGS
 
-# flags.DEFINE_string('framework', 'tf', '(tf, tflite, trt')
-# flags.DEFINE_string('weights', './checkpoints/yolov4-416', 'path to weights file')
-flags.DEFINE_string('video', 'E:/Didban/Detectron2/CompleteDetectron2/videos/mall.mp4', 'path to input video or set to 0 for webcam')
-flags.DEFINE_string('output', None, 'path to output video')
-flags.DEFINE_integer('class_id', 0, 'class_id number to')
+flags.DEFINE_string('video', '/content/Detectron2_Watcher/example/Car_test_1.mp4', 'path to input video or set to 0 for webcam')
+flags.DEFINE_string('output', '/content/Detectron2_Watcher/Results/Car_test_1_result.mp4', 'path to output video')
+flags.DEFINE_integer('class_id', 2, 'class_id number to')
 flags.DEFINE_integer('size', 416, 'resize images to')
 flags.DEFINE_string('model', 'COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml', 'COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml')
 flags.DEFINE_string('output_format', 'XVID', 'codec used in VideoWriter when saving video to file')
-# flags.DEFINE_float('iou', 0.45, 'iou threshold')
 flags.DEFINE_float('score', 0.50, 'score threshold')
-flags.DEFINE_integer('polygon',[1632, 1654], [2920, 1622], 'polygon threshold')
+flags.DEFINE_string('polygon','[788, 286],[140, 314],[376, 170],[496, 166],[788, 282]' , 'polygon threshold')
 
 def main(argv):
-    # input_size = FLAGS.size
     source_path = FLAGS.video
     target_path = FLAGS.output
     class_id = FLAGS.class_id
     input_size = FLAGS.size
     model = FLAGS.model
+    polygon = FLAGS.polygon
     
-    # initiate polygon zone Grand # ************* flags
-    polygon = np.array([
-             [1632, 1654],
-             [2920, 1622]
-    ])
+    # initiate polygon zone cars
+    res = ast.literal_eval(polygon)
+    res = list(res)
+ 
+    polygon = np.array(res)
 
 
     video_info = sv.VideoInfo.from_video_path(source_path)
     zone = sv.PolygonZone(polygon=polygon, frame_resolution_wh=video_info.resolution_wh)
 
     # initiate annotators
-    box_annotator = sv.BoxAnnotator(thickness=4, text_thickness=4, text_scale=2)
-    zone_annotator = sv.PolygonZoneAnnotator(zone=zone, color=sv.Color.white(), thickness=6, text_thickness=6, text_scale=4)
+    box_annotator = sv.BoxAnnotator(thickness=1, text_thickness=1, text_scale=0.5)
+    zone_annotator = sv.PolygonZoneAnnotator(zone=zone, color=sv.Color.white(), thickness=2, text_thickness=2, text_scale=1)
 
 
     emp1 = predictor(model)
@@ -78,9 +75,9 @@ def main(argv):
 
         return  frame
 
-    ESCALATOR_VIDEO_PATH =  r'E:/Didban/Detectron2/CompleteDetectron2/videos/mall.mp4' 
+   Cars_VIDEO_PATH =  r'/content/Detectron2_Watcher/example/Car_test_1.mp4'
 
-    sv.process_video(source_path=ESCALATOR_VIDEO_PATH, target_path = target_path, callback=process_frame)
+    sv.process_video(source_path = cars_VIDEO_PATH, target_path = target_path, callback=process_frame)
 
     
     # display.clear_output()
